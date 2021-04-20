700ceacad0e790a9985cb9f7691611ed0e4bd454
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-04-20 21:40:26 +0200

    iterate recursively

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
 const { logger } = require("./lib");
 const { Measure } = require("./models/Measure");
 const { parseCSV } = require("./parser");
 
+dotenv.config();
+
+const DIR_PATH = process.env.DIR_PATH;
+
 const start = async () => {
-  const measures = await parseCSV("./data/sample.csv");
+  const files = [];
+  walkSync(DIR_PATH, files);
+  const csvFiles = files.filter((file) => file.endsWith(".csv"));
+
+  for (let file of csvFiles) {
+    logger.info(`start processing ${file}`);
+    await processCSV(file);
+    logger.info(`end processing ${file}`);
+  }
+
+  process.exit(0);
+};
+
+try {
+  start();
+} catch (err) {
+  logger.error(err);
+}
+
+async function processCSV(filepath) {
+  const measures = await parseCSV(filepath);
 
   for (let measure of measures) {
     const sameMeasure = await Measure.query().findOne({
@@ -15,11 +41,21 @@ const start = async () => {
     }
   }
 
-  process.exit(0);
-};
+  fs.unlinkSync(filepath);
+  logger.info(`file deleted ${filepath}`);
+}
 
-try {
-  start();
-} catch (err) {
-  logger.error(err);
+function walkSync(dir, filelist) {
+  if (dir[dir.length - 1] != "/") dir = dir.concat("/");
+
+  files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function (file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + "/", filelist);
+    } else {
+      filelist.push(dir + file);
+    }
+  });
+  return filelist;
 }
